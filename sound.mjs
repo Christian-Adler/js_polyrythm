78181fea8cb3c34fe042fd34f4226f47bb27f082
Christian Adler <chris.adler@gmx.de>
2024-08-16 17:45:49 +0200

    improve sound for multiple played sounds at a time

--- sound.mjs
+++ sound.mjs
@@ -1,13 +1,14 @@
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-const playSound = (frequency = 440, duration = 1) => {
+const playSound = (frequency = 440, duration = 2) => {
   const osc = audioCtx.createOscillator();
   const envelope = audioCtx.createGain();
   osc.connect(envelope);
   envelope.connect(audioCtx.destination);
 
   envelope.gain.setValueAtTime(0, audioCtx.currentTime);
-  envelope.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 0.05);
+  // low value because of overlapping sounds at a time
+  envelope.gain.linearRampToValueAtTime(0.05, audioCtx.currentTime + 0.05);
   envelope.gain.linearRampToValueAtTime(0, audioCtx.currentTime + duration - 0.05);
 
   osc.frequency.setValueAtTime(frequency, audioCtx.currentTime);
