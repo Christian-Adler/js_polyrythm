87af3196ae7a00c1c5f1569d450faa0b072bfc9e
Christian Adler <chris.adler@gmx.de>
2024-08-16 17:21:20 +0200

    add change ball direction

--- ball.mjs
+++ ball.mjs
@@ -4,11 +4,14 @@ class Ball {
     this.radius = radius;
     this.speed = speed;
     this.offset = 0;
+    this.direction = 1;
     this.center = {x: 0, y: 0};
   }
 
   draw(ctx) {
     this.center = this.track.getPosition(this.offset);
+    if (this.center.y > this.track.center.y)
+      this.direction *= -1;
     ctx.beginPath();
     ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2, false);
     ctx.strokeStyle = 'white';
@@ -16,7 +19,7 @@ class Ball {
   }
 
   move() {
-    this.offset += this.speed;
+    this.offset += this.speed * this.direction;
   }
 }
 
