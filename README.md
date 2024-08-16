734d59c6482d3b152bf5c9e309864da930d51cd1
Christian Adler <chris.adler@gmx.de>
2024-08-16 16:43:20 +0200

    add basic setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # js_polyrythm
+
+Tutorial from https://www.youtube.com/watch?v=FFyuRzq0nsg
+
+![Preview](https://github.com/Christian-Adler/js_polyrythm/blob/main/preview.jpg?raw=true)
---
+++ main.mjs
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("canvasId");
+const ctx = canvas.getContext('2d');
+
+let worldWidth = canvas.width;
+let worldHeight = canvas.height;
+let worldWidth2 = worldWidth / 2;
+let worldHeight2 = worldHeight / 2;
+let worldUpdated = true;
+
+const updateWorldSettings = () => {
+  if (worldHeight !== window.innerHeight || worldWidth !== window.innerWidth) {
+    worldWidth = window.innerWidth;
+    worldHeight = window.innerHeight;
+    worldWidth2 = worldWidth / 2;
+    worldHeight2 = worldHeight / 2;
+    canvas.width = worldWidth;
+    canvas.height = worldHeight;
+    worldUpdated = true;
+  }
+};
+
+updateWorldSettings();
